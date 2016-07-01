a8fa4afdfe2765a0aaefcc97d9913b16704a6b28
rago <rago@google.com>
2016-07-01 16:05:00 -0700

    universal7580: Set voice call volume steps to 7
    Bug: 28426597
    Change-Id: Icf78d9acec75c500982b1e4391af4d6f3933da37

--- system_prop.mk
+++ system_prop.mk
@@ -18,7 +18,8 @@
 PRODUCT_PROPERTY_OVERRIDES += \
     af.fast_track_multiplier=1 \
     audio_hal.force_voice_config=wide \
-    ro.config.media_vol_steps=25
+    ro.config.media_vol_steps=25 \
+    ro.config.vc_call_vol_steps=7
 
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
